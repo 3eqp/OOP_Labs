e9ba2d363d82f0600dcc485b4e8b1fc6da3d6c5d
Ivan <Ivanzenkovich@mail.ru>
2016-09-19 21:30:22 +0300

    blabla

---
+++ Lab2/Lab2/Lab2.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "triangle.h"
+#include <iostream> 
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[]) {
+	Triangle A;
+	int x = 0, y = 0;
+	A.setCoordinates(x, y);
+	A.calculations(x1, y1, x2, y2)
+		A.print();
+	return 0;
+}
